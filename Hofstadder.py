9727ca285fff9e1ea24b525e19655cbba66789d2
Ícaro Targino <icaro.rique@eng.ci.ufpb.br>
2018-04-22 20:14:32 -0300

    Add files via upload

--- Hofstadder.py
+++ Hofstadder.py
@@ -5,12 +5,36 @@ def hofstadder(nval):
     if nval == 1 or nval == 2:
         return 1
     else:
+        print("VALOR DE nval", nval)
         return hofstadder(hofstadder(nval-1))+hofstadder(nval-hofstadder(nval-1))
 
+def hofstadder1(nval):
 
+    if nval == 1 or nval == 2:
+        return 1
+    else:
+        print("valor de Nval hof1", nval)
+        return hofstadder1(hofstadder1(nval-1))
+
+def hofstadder2(nval):
+
+    if nval == 1 or nval == 2:
+        return 1
+    else:
+        print("valor de Nval hof2", nval)
+        return hofstadder2(nval-hofstadder2(nval-1))
 
 print("Sequencia de Hofstadder")
 n = int(input("Valor para n, buscamos -- a(n) = a(a(n-1)) + a(n-a(n-1)) --"))
 
-prim = hofstadder(n)
-print ("NUMERO QUERIDO : ",prim)
+val = hofstadder(n)
+prim = hofstadder1(n)
+seg = hofstadder2(n)
+
+asd = prim + seg
+#val2 = prim+seg
+print ("NUMERO QUERIDO : ",val)
+print ("PRIMEIRO", prim)
+print ("SEGUNDO", seg)
+print ("asd",asd)
+#print ("SOMA DOS DOIS : ",val2)
--- Hofstadder.s
+++ Hofstadder.s
@@ -4,8 +4,8 @@ msg0: .asciiz "\n - Grupo:\n\t- Icaro Targino;\n\t- Bruno Gomes;\n\t- Kaio Moura
 msg1: .asciiz "\t *************************************\n\t ****** Sequencia de Hofstadder ******\n\t *************************************\n"
 msg2: .asciiz "\t - TRABALHO DE ARQUITETURA -\t - PROFESSOR: YURI GONZAGA - "
 msg3: .asciiz "\n\tEscreva o numero que você quer :\n\t"
-msg4: .asciiz "\n IGUAL A 1 OU 2\n"
-msg5: .asciiz "\n NAO E IGUAL A 1 OU 2\n"
+msg4: .asciiz "\n"
+msg6: .asciiz "\n VALOR FINAL \n:"
 
 .text
 .globl main         #MAIN()
@@ -31,31 +31,93 @@ main:               #PONTO DE ENTRADA
     syscall         #CHAMADA DO SISTEMA
 
     add $a0, $v0, $zero      #VALOR LIDO FICA EM $a0
-    jal Hofstadder          #CHAMADA DA FUNÇÃO
+
+    #addi $s0, $v0, 1       #SOMA O VALOR EM $v0 COM 1 E ARMAZENA EM $s0
+    #add $a0, $zero, 1     #SETA VALOR DE $a0 COMO 1
+    jal Hofstadder
+    #j Loop          #CHAMADA DA FUNÇÃO
+
     add $a0, $v0, $zero     #MOVE-SE ENTRE REGISTRADORES - $ZERO É A CONSTANTE 0
+
+    li $v0, 1       #IMPRIME RESULTADO FINAL
+    #la $a0, ($a1)
+    syscall
+
+    li $v0, 4       #COMANDO DE IMPRESSÃO DE UMA STRING NA TELA
+    la $a0, msg6    #IMPRIME MENSAGEM ($a0 É O REGISTRADOR QUE IRÁ CONTER O VALOR A SER IMPRESSO)
+    syscall         #CHAMADA DO SISTEMA
 
     li $v0, 10      # COMANDO DE EXIT
     syscall         # EFETUA CHAMADA AO SISTEMA
 
+#Loop:                   #REPETE
+#    beq $a0, $s0, End
+#    jal Hofstadder
+
+#    add $a2, $a0, $zero
+#    add $a0, $zero, $v0
+
+#    addi $v0, $zero, 1
+#    syscall
+
+#    li $v0, 4            #pula linha
+#    la $a0, msg4
+#    syscall
+
+#    add $a0, $a2, $zero
+#    addi $a0, $a0, 1
+#    j Loop
+#    jr $ra
+
+
 Hofstadder:         #TAG DA FUNÇÃO
+
+    addi $sp, $sp, -12
+    sw $ra, 8($sp)
+    sw $a1, 4($sp)
+    sw $a0, 0($sp)
 
     beq $a0, 1, L1  #SE $a0 == 1 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
     beq $a0, 2, L1  #SE $a0 == 2 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
 
-    li $v0, 4       #COMANDO DE IMPRESSÃO DE UMA STRING NA TELA
-    la $a0, msg5    #IMPRIME MENSAGEM ($a0 É O REGISTRADOR QUE IRÁ CONTER O VALOR A SER IMPRESSO)
-    syscall         #CHAMADA DO SISTEMA
+    #li $v0,1        #CHAMADA QUE IMPRIME UM INTEIRO
+    #syscall
 
-    add       $a1, $a0, $zero    #a1 = a0 :: ATRIBUIÇÃO PARA OUTRA VARIÁVEL DE MODO A MANTER O VALOR
-    addi      $a0, $a0 , -1      #a0 = a0 -1 :: DECREMENTAÇÃO
+    add $a1, $a0, $zero         #a1 = a0 , vamos precisar do valor incicial de n
+    addi $a0, $a0, -1          #a0 = a0 -1
 
+    #primeira parte
+    jal Hofstadder              #a(n-1)
+    add $a0, $v0, $zero         #n = return de a(n-1)
+    jal Hofstadder              #a(a(n-1))
+    add $a0, $v0, $zero         #return de a(a(n-1))
+    add $s3, $a0, $zero
 
+    #segunda parte
+    jal Hofstadder              #a(n-1)
+    add $a0, $v0, $zero         #n = return de a(n-1)
+    sub $a0, $a1, $a0           #return n-a(n-1)))
+    jal Hofstadder              #a(n-a(n-1))
+    add $a0, $v0, $zero         #return(a(n-a(n-1)))
 
-    jr $ra          #RETURN (RETORNA PARA O LUGAR DE ORIGEM ONDE FOI CHAMADO)
+    add $s4, $s3, $a0           #Soma H(1)+H(2)
+
+    add $v0, $s4, $zero
+
+    j EXIT
 
 L1:
-    li $v0, 4       #COMANDO DE IMPRESSÃO DE UMA STRING NA TELA
-    la $a0, msg4    #IMPRIME MENSAGEM ($a0 É O REGISTRADOR QUE IRÁ CONTER O VALOR A SER IMPRESSO)
-    syscall         #CHAMADA DO SISTEMA
 
-    jr $ra          #RETURN (RETORNA PARA O LUGAR DE ORIGEM ONDE FOI CHAMADO)
+    add $v0, $zero, 1
+
+
+EXIT:
+
+    lw $a0, 0($sp)
+    lw $a1, 4($sp)
+    lw $ra, 8($sp)
+    addi $sp, $sp, 12
+    jr $ra
+
+End:
+    jr $ra
