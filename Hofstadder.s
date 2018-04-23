28867928f691508ad31b0f6cde5dadf82641e502
Ícaro Targino <icaro.rique@eng.ci.ufpb.br>
2018-04-23 15:53:33 -0300

    Add files via upload

--- Hofstadder.s
+++ Hofstadder.s
@@ -2,7 +2,7 @@
 .data
 msg0: .asciiz "\n - Grupo:\n\t- Icaro Targino;\n\t- Bruno Gomes;\n\t- Kaio Moura;\n\t- Artur Curty;\n\t- Giovanni Carvalho.\n"
 msg1: .asciiz "\t *************************************\n\t ****** Sequencia de Hofstadder ******\n\t *************************************\n"
-msg2: .asciiz "\t - TRABALHO DE ARQUITETURA -\t - PROFESSOR: YURI GONZAGA - "
+msg2: .asciiz "\t - TRABALHO DE ARQUITETURA -\t - PROFESSOR: YURI GONZAGA - NOTA DO GRUPO 10"
 msg3: .asciiz "\n\tEscreva o numero que você quer :\n\t"
 msg4: .asciiz " "
 msg6: .asciiz "\n VALOR FINAL \n:"
@@ -53,21 +53,23 @@ Loop:                       # TAG DO LOOP
     add $a0, $a2, $zero
     addi $a0, $a0, 1        #INCREMENTO DE N
     j Loop                  #ENVIA A EXECUÇÃO PARA O INICIO DO LOOP - TAG -
-    jr $ra
+    #jr $ra
 
 
 Hofstadder:                 #TAG DA FUNÇÃO
 
-    addi $sp, $sp, -12      #COLOCANDO AS VARIÁVEIS NA PILHA
+    addi $sp, $sp, -16      #COLOCANDO AS VARIÁVEIS NA PILHA
     sw $ra, 8($sp)
     sw $a1, 4($sp)
     sw $a0, 0($sp)
+    sw $s3, 12($sp)
 
     beq $a0, 1, NAO_SATISFAZ          #SE $a0 == 1 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
     beq $a0, 2, NAO_SATISFAZ          #SE $a0 == 2 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
 
-    move $a1, $a0           #a1 = a0 - VOLTAREMOS A USAR O VALOR DE N
-    addi $a0, $a0, -1       #a0 = a0 -1
+    move $a1, $a0           #$a1 = $a0 - VOLTAREMOS A USAR O VALOR DE N
+    addi $a0, $a0, -1       #$a0 = $a0 -1
+
     j PRIMEIRA_PARTE        #CHAMADA DA FUNÇÃO
 
 PRIMEIRA_PARTE:             #TAG DA FUNÇÃO
@@ -80,8 +82,9 @@ PRIMEIRA_PARTE:             #TAG DA FUNÇÃO
     j SEGUNDA_PARTE         #CHAMADA DA FUNÇÃO
 
 SEGUNDA_PARTE:              #TAG DA FUNÇÃO
+    addi $a0, $a1, -1       #$a0 = $a1 -1
+
     jal Hofstadder          #a(n-1)
-    move $a0, $v0           #n = return de a(n-1)
     sub $a0, $a1, $v0       #return n-a(n-1)
     jal Hofstadder          #a(n-a(n-1))
     move $a0, $v0           #return a(n-a(n-1))
@@ -98,7 +101,8 @@ EXIT:
     lw $a0, 0($sp)
     lw $a1, 4($sp)
     lw $ra, 8($sp)
-    addi $sp, $sp, 12       #DESALOCA A PILHA
+    lw $s3, 12($sp)
+    addi $sp, $sp, 16       #DESALOCA A PILHA
 
     jr $ra
 
