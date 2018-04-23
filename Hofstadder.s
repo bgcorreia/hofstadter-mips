66ff9d853cce427f09cdfb39dfd63b0add6d6f1f
Ícaro Targino <icaro.rique@eng.ci.ufpb.br>
2018-04-23 12:37:04 -0300

    Add files via upload

--- Hofstadder.s
+++ Hofstadder.s
@@ -35,26 +35,15 @@ main:                       #PONTO DE ENTRADA
 
     j Loop                  #CHAMADA DA FUNÇÃO
 
-    move $a0, $v0           #MOVE-SE ENTRE REGISTRADORES - $ZERO É A CONSTANTE 0
-
-    #li $v0, 1              #SOMA O VALOR EM $v0 COM 1 E ARMAZENA EM $s0
-    #la $a0, ($a1)
-    #syscall
-
-    #li $v0, 4               #COMANDO DE IMPRESSÃO DE UMA STRING NA TELA
-    #la $a0, msg6            #IMPRIME MENSAGEM ($a0 É O REGISTRADOR QUE IRÁ CONTER O VALOR A SER IMPRESSO)
-    #syscall                 #CHAMADA DO SISTEMA
-
-    #li $v0, 10              # COMANDO DE EXIT
-    #syscall                 # EFETUA CHAMADA AO SISTEMA
+    move $a0, $v0           #MOVE-SE ENTRE REGISTRADORES
 
 Loop:                       # TAG DO LOOP
-    beq $a0, $s0, End
-    jal Hofstadder
+    beq $a0, $s0, End       #VERIFICAÇÃO DE SATISFAÇÃO DE CONDIÇÃO n == n
+    jal Hofstadder          #CHAMADA DA FUNÇÃO
 
-    move $a2, $a0
-    move $a0, $v0
-    addi $v0, $zero, 1
+    move $a2, $a0           #PÕE O VALOR DO REGISTRADOR $a0 EM $a2
+    move $a0, $v0           #PÕE O RETORNO DA FUNÇÃO NO $a0
+    li $v0,1
     syscall
 
     li $v0, 4               #COMANDO DE IMPRESSÃO DE UMA STRING NA TELA - ESPAÇO ENTRE OS NUMEROS
@@ -62,35 +51,35 @@ Loop:                       # TAG DO LOOP
     syscall                 #CHAMADA DO SISTEMA
 
     add $a0, $a2, $zero
-    addi $a0, $a0, 1
+    addi $a0, $a0, 1        #INCREMENTO DE N
     j Loop                  #ENVIA A EXECUÇÃO PARA O INICIO DO LOOP - TAG -
     jr $ra
 
 
 Hofstadder:                 #TAG DA FUNÇÃO
 
-    addi $sp, $sp, -12
+    addi $sp, $sp, -12      #COLOCANDO AS VARIÁVEIS NA PILHA
     sw $ra, 8($sp)
     sw $a1, 4($sp)
     sw $a0, 0($sp)
 
-    beq $a0, 1, L1          #SE $a0 == 1 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
-    beq $a0, 2, L1          #SE $a0 == 2 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
+    beq $a0, 1, NAO_SATISFAZ          #SE $a0 == 1 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
+    beq $a0, 2, NAO_SATISFAZ          #SE $a0 == 2 (SATISFAZ E VAI PRA L1) ou $a0 != (NÃO SATISFAZ E CONTINUA)
 
-    move $a1, $a0           #a1 = a0 , vamos precisar do valor incicial de n
+    move $a1, $a0           #a1 = a0 - VOLTAREMOS A USAR O VALOR DE N
     addi $a0, $a0, -1       #a0 = a0 -1
-    j primeira
+    j PRIMEIRA_PARTE        #CHAMADA DA FUNÇÃO
 
-primeira:                   #parte
+PRIMEIRA_PARTE:             #TAG DA FUNÇÃO
     jal Hofstadder          #a(n-1)
     move $a0, $v0           #n = return de a(n-1)
     jal Hofstadder          #a(a(n-1))
     move $a0, $v0           #return de a(a(n-1))
     move $s3, $a0           #REGISTRA EM $s3 O VALOR DE $a0
 
-    j segunda
+    j SEGUNDA_PARTE         #CHAMADA DA FUNÇÃO
 
-segunda:                    #parte
+SEGUNDA_PARTE:              #TAG DA FUNÇÃO
     jal Hofstadder          #a(n-1)
     move $a0, $v0           #n = return de a(n-1)
     sub $a0, $a1, $v0       #return n-a(n-1)
@@ -103,16 +92,14 @@ segunda:                    #parte
 
     j EXIT
 
-L1:
-
-    add $v0, $zero, 1
-
+NAO_SATISFAZ:
+    add $v0, $zero, 1       #RETORNA O VALOR 1 NA VARIAVEL $v0
 EXIT:
-
     lw $a0, 0($sp)
     lw $a1, 4($sp)
     lw $ra, 8($sp)
-    addi $sp, $sp, 12
+    addi $sp, $sp, 12       #DESALOCA A PILHA
+
     jr $ra
 
 End:
